babb0cc1d508676e1f4d4485802002b71ec94507
bikashroy120 <bikashroydt@gmail.com>
2023-07-10 12:20:04 +0600

    filter and search add

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 
+import CarCard from '@/components/CarCard'
 import CustomFilter from '@/components/CustomFilter'
 import Hero from '@/components/Hero'
 import SearchBar from '@/components/SearchBar'
+import ShowMore from '@/components/ShowMore'
+import { fuels, yearsOfProduction } from '@/constants'
+import { HomeProps } from '@/types'
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
 
   return (
@@ -24,15 +34,23 @@ export default async function Home() {
         <div className='home__filters'>
             <SearchBar />
             <div className='home__filter-container'>
-              <CustomFilter title="fuel"/>
-              <CustomFilter title="year"/>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
             </div>
         </div>
 
         {
           !isDataEmpty ? (
             <section>
-              WE Have cars
+              <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
             </section>
           ):(
             <div className='home__error-container'>
--- components/CustomButon.tsx
+++ components/CustomButon.tsx
@@ -1,20 +1,29 @@
 "use client"
 
 import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
 import React from 'react'
 
-const CustomButon = ({title,containerStyles,handleClick}:CustomButtonProps) => {
+const CustomButon = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => {
   return (
     <button
-        disabled={false}
-        type={'button'}
-        className={`custom-btn  ${containerStyles}`}
-        onClick={handleClick}
-    >
-        <span className={`flex-1`}>
-            {title}
-        </span>
-    </button>
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
   )
 }
 
